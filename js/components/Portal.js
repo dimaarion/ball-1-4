f90e5aacedc9ed37ef74775f74211b89f180fee5
Dmitriy <48031182+dimaarion@users.noreply.github.com>
2023-07-09 22:54:52 +0000

    Update 8 files

--- js/components/Portal.js
+++ js/components/Portal.js
@@ -1,27 +1,46 @@
 class Portal extends Body {
-    animate = new Animate();
-    scale = 8;
-    constructor(props){
-        super(props)
-    }
-    preload() {
-        this.animate.animateE("./asset/portal/portal.png");
-    }
+  animate = new Animate();
+  animate2 = new Animate();
+  scale = 8;
+  constructor(props) {
+    super(props);
+  }
+  preload() {
+    this.animate.animateE("./asset/portal/portal.png");
+    this.animate2.animateE("./asset/portal/portal2.png");
+  }
 
-    create(world, scena) {
-        this.animate.setupAnimate();
-        this.sensor = true;
-        this.createRect(world, scena);
+  create(world, scena) {
+    this.animate.setupAnimate();
+    this.sensor = true;
+    this.createRect(world, scena);
 
-        this.body.map((b)=>{
-           
-        })
-        console.log(this.body)
-    }
+    this.body.map((b) => {});
+    console.log(this.body);
+  }
 
-    view(){
-       this.body.map((b)=> image(this.animate.sprite(),b.position.x - b.width / 2,b.position.y - b.height / 2,b.width,b.height))
-       
-    }
-
+  view() {
+    this.body
+      .filter((f) => f.typeObject === "vertical")
+      .map((b) =>
+        image(
+          this.animate.sprite(),
+          b.position.x - b.width / 2,
+          b.position.y - b.height / 2,
+          b.width,
+          b.height
+        )
+      );
+    this.body
+      .filter((f) => f.typeObject === "gorizontal")
+      .map((b) =>
+        image(
+          this.animate2.sprite(),
+          b.position.x - b.width / 2,
+          b.position.y - b.height / 2,
+          b.width,
+          b.height
+        )
+      );
+  }
 }
