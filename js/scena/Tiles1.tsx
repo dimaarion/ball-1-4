d55fb211d02027764bc9253daa27e2adfa0e9986
dimaarion <dimaarion@gmail.com>
2023-09-14 23:42:00 +0300

    dimaarion/ball-1-4

--- js/scena/Tiles1.tsx
+++ js/scena/Tiles1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="Tiles1" tilewidth="32" tileheight="32" tilecount="225" columns="15">
- <image source="../../asset/level1/Tiles1.png" width="500" height="500"/>
+<tileset version="1.8" tiledversion="1.8.4" name="Tiles1" tilewidth="32" tileheight="32" tilecount="324" columns="18">
+ <image source="../../asset/level1/Tiles1.png" width="600" height="600"/>
 </tileset>
